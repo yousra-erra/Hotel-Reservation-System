40fe51d6b2ed3890ec3a344c5dab59829393650d
yousra-errami <erramiyousra7@gmail.com>
2026-05-31 17:47:11 +0100

    Fix database connection for Clever Cloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,8 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 3306,
+    ssl: { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
