67e23502cb392eb7ce52db6d35d2f0dc160a4083
yousra-errami <erramiyousra7@gmail.com>
2026-05-31 18:03:20 +0100

    Fix MySQL connection pool for Vercel serverless

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,20 +2,24 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
     if (err) {
         console.log(err);
     } else {
         console.log('MySQL Connected');
+        connection.release();
     }
 });
 
